dcac783398c5fe20d126582f7dc7bc4e9743fb0c
Mikay <issamikaela@gmail.com>
2026-02-04 23:41:10 +0800

    Add job creation endpoint with employer middleware

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
+const employerOnly = require("./middleware/employerOnly");
 
+app.post("/jobs/create", employerOnly, (req, res) => {
+  res.send("Job created successfully");
+});
+
+app.use("/jobs", require("./routes/jobs"));
