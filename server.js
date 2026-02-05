8a15d44462b05b9dbbf97135ac68615f87cecd77
Mikay <issamikaela@gmail.com>
2026-02-05 12:43:23 +0800

    Implement user role upgrade to employer
    Added endpoint to upgrade user role to employer.

--- server.js
+++ server.js
@@ -5,3 +5,29 @@ app.post("/jobs/create", employerOnly, (req, res) => {
 });
 
 app.use("/jobs", require("./routes/jobs"));
+
+// server.js
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Mock database
+let users = [
+  { id: 1, role: "regular" }
+];
+
+app.post("/api/upgrade-to-employer", (req, res) => {
+  const user = users[0]; // sample logged-in user
+
+  user.role = "employer";
+
+  res.status(200).json({
+    message: "User upgraded to Employer"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
